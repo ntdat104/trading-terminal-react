37346e7097319d1db9411ec1660edfb15eca3905
Tiến Đạt <ntd.k108cva@gmail.com>
2024-06-07 13:19:58 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,7 @@ import React from "react";
 import Chart from "./components/Chart";
 
 const App: React.FC = (): JSX.Element => {
-  return (
-    <div>
-      <Chart />
-    </div>
-  );
+  return <Chart />;
 };
 
 export default App;
