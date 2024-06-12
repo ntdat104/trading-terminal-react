e43f8bcdab30ca708f8d3bd449ba8b3020f3fd9e
Tiến Đạt <ntd.k108cva@gmail.com>
2024-06-12 18:09:03 +0700

    update format

--- src/components/format-price.ts
+++ src/components/format-price.ts
@@ -35,11 +35,32 @@ export const formatPrice = (value: number | string) => {
 
   let digit: number;
 
-  if (absValue < 1) {
+  if (absValue < 0.000001) {
+    // 0.0000009876
+    digit = 10;
+  } else if (absValue < 0.00001) {
+    // 0.000009876
+    digit = 9;
+  } else if (absValue < 0.0001) {
+    // 0.00009876
+    digit = 8;
+  } else if (absValue < 0.001) {
+    // 0.0009876
+    digit = 7;
+  } else if (absValue < 0.01) {
+    // 0.009876
+    digit = 6;
+  } else if (absValue < 0.1) {
+    // 0.09876
+    digit = 5;
+  } else if (absValue < 1) {
+    // 0.9876
     digit = 4;
   } else if (absValue < 10) {
+    // 9.876
     digit = 3;
   } else {
+    // 98.76
     digit = 2;
   }
 
