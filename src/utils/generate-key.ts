01c0df3bc2c9fb893ede5417fff02c0bb2c9803b
ntdat104 <ntd.k108cva@gmail.com>
2025-11-06 13:11:47 +0700

    fix generateCacheKey

--- src/utils/generate-key.ts
+++ src/utils/generate-key.ts
@@ -1,14 +1,14 @@
-import { createHash } from "crypto";
-
-export const generateCacheKey = (
+export const generateCacheKey = async (
   url: string,
   method: "GET" | "POST" = "GET",
   timeout: number,
   disable: boolean,
   body?: any
 ) => {
-  const bodyHash = body
-    ? createHash("sha256").update(JSON.stringify(body)).digest("hex")
-    : "";
+  const encoded = new TextEncoder().encode(JSON.stringify(body || ""));
+  const hashBuffer = await crypto.subtle.digest("SHA-256", encoded);
+  const hashArray = [...new Uint8Array(hashBuffer)];
+  const bodyHash = hashArray.map(b => b.toString(16).padStart(2, "0")).join("");
+
   return `${method}:${url}:${bodyHash}:${timeout}:${disable.toString()}`;
 };
